9e62c3a57d4dd166b157e4cc8bb6f307dcd5fef6
Giovanna <giovannagmoeller@gmail.com>
2022-03-05 20:19:40 -0300

    installing react router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
+import Details from './pages/Details';
 import './global.css'
+ 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/details" element={<Details/>}/>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
